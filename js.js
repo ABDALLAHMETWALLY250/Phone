0ee173c2691a00a3ef2d324be2c72598f4a53cb8
ABDALLAHMETWALLY250 <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2022-09-25 14:34:35 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".im");
+let contanier = document.querySelector(".contanier");
+function phones(phone) {
+  img.src = phone;
+}
+function colors(color)
+{
+    contanier.style.background = color;
+}
